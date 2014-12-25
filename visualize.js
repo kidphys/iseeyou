2d6607cb4b24134189253978da65715b9c65368d
chau.hoang <chau.hoang@vndirect.com.vn>
2014-12-25 16:55:24 +0700

    See prices axis

--- visualize.js
+++ visualize.js
@@ -1,36 +1,50 @@
+    function addAxis(chart, width, height, prices){
+        var x = d3.scale.ordinal()
+                .domain(prices)
+                .rangeRoundBands([0, width]);
+        var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+        chart.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + height  + ')')
+            .call(xAxis);
+    }
+
     function update(chart, data){
         var barWidth = width / data.length;
 
-        var group = chart.selectAll('g') // select group
+        var group = chart.selectAll('.bar') // select group
         .data(data)
 
+        var bar = group.enter()
+            .append('g')
+            .attr('transform', function(d, i){
+                return "translate(" + i * barWidth + ",0)"; // this is the property that translates the bar
+        });
+
         function addRect(group){
-            bar.attr('class', function(d){return d.class;});
-            bar.append('rect')
+            group.attr('class', function(d){return 'bar ' + d.class;});
+            group.append('rect')
             .attr('y', function(d){return y(d.value)})
             .attr('height', function(d){ return height - y(d.value);})
             .attr('width', barWidth - 1);
 
-            bar.append('text')
+            group.append('text')
             .attr('x', barWidth / 2 + 10)
             .attr('y', function(d){return y(d.value) - 10;}) // scale value to auto-fit
             .attr('dy', '.75em')
             .text(function(d){return d.value;});
         }
 
-        var bar = group.enter()
-            .append('g')
-            .attr('transform', function(d, i){
-        return "translate(" + i * barWidth + ",0)"; // this is the property that translates the bar
-        });
 
         addRect(bar);
 
-        group.attr('class', function(d){return d.class;});
-        group.select('rect').transition()
+        group.attr('class', function(d){return 'bar ' + d.class;});
+        group.select('rect') //.transition()
             .attr('y', function(d){return y(d.value)})
             .attr('height', function(d){ return height - y(d.value);})
-        group.select('text').transition()
+        group.select('text') //.transition()
             .attr('x', barWidth / 2 + 10)
             .attr('y', function(d){return y(d.value) - 10;}) // anonymous works on each value
             .attr('dy', '.75em')
@@ -38,6 +52,7 @@
 
         group.exit().remove();
     }
+
 
     /**
      * [virtual_play description]
@@ -47,6 +62,7 @@
                 .timeFormat: used to parse start_time & data[i].time
                 .timeStep: how much to move forward in seconds
                 .process: optional function to process data before yield
+                .breakTime: {start: '11:30', 90 } // minutes
      * @return {[type]}          [description]
      */
     function virtual_play(data, settings){
@@ -55,9 +71,11 @@
             process = settings.process;
         }
 
+        var breakTime = settings.breakTime;
+
         var timeStep = settings.timeStep;
         var stock = process(data[0]);
-        var now = moment(settings.start_time, settings.time_format);
+        var now = moment(settings.startTime, settings.timeFormat);
         var i = 0;
         var iter = {
             next: function(){
@@ -78,7 +96,13 @@
                     };
                 }
                 else{
-                    now.add(timeStep, 'seconds');
+                    // hack for passing noon time
+                    if(now.format(breakTime.format) == breakTime.start){
+                        now.add(breakTime.duration, 'minutes');
+                    }
+                    else{
+                        now.add(timeStep, 'seconds');
+                    }
                     return {
                         stock: null,
                         time: moment(now)
